55ae44842bbfb9371174d9447221c04ee86eadb3
Harsh aka Alpha <30981004+harshpv07@users.noreply.github.com>
2020-12-29 15:19:14 +0530

    final

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from threading import Thread
 
 class store:
     defloc = "" 
+    del_cnt = 0
     def __init__(self ,  loc): #Constructor for file location
         if(os.path.exists(loc) == False): #Create a new file if not found
             with open(loc , 'w') as data:
@@ -29,13 +30,11 @@ class store:
                     a = datetime.datetime(j["year"] , j["month"] ,j["date"] ,j["hour"] ,j["minute"] ,j["seconds"]) #Get the time of insertion from the 
                     if(datetime.datetime.now() > a + datetime.timedelta(seconds = j["ttl"])): #Check for the condition with current time,ttl and time of insertion
                         self.delete(i)
+                        self.del_cnt -= 1
                         print(str(i) + " " + "Sucessfully deleted using TTL")
-                    # else:
-                    #     print(str(i) + " " + "no yet")
-                # print(a , end = " ")
-                # print(j["ttl"] , end = " ")
-                # print()
-
+            if(self.del_cnt == 0): #Break and Kill thread when all TTL Keys have expired
+                print("______All operations done______")
+                break
                 
     
     
@@ -56,6 +55,8 @@ class store:
                         for i,j in json_decoded.items():
                             dic[i] = j
                         if str(key) not in dic:
+                            if("ttl" in value):
+                                self.del_cnt += 1
                             dic[key] = value 
                             dic[key]["year"] = datetime.datetime.now().year # Insert a mandatory key-val pair for time properties (time of insertion)
                             dic[key]["month"] = datetime.datetime.now().month
@@ -65,29 +66,30 @@ class store:
                             dic[key]["seconds"] = datetime.datetime.now().second
                             with open(self.defloc, 'w') as fp:
                                 json.dump(dic, fp) #dump key-val pairs into .json file
-                            print("Sucessfully inserted")
+                            print(str(key) + " " + "sucessfully inserted")
                         else:
-                            print("Duplicate key found") #If duplicate key is found 
+                            print("Duplicate key " + str(key)) #If duplicate key is found 
                             pass
 
 
-        #print(dic)
+        #print(self.del_cnt)
+
     def read(self , key): #Return the value
         with open(self.defloc, 'r') as data_file: #Read the .json file
             keyss = json.load(data_file)
         if(key in keyss): #Check if key is present
             print(keyss[key]) #return value of key
         else:
-            print("Key not found") #If key not found 
+            print("Key " + str(key) + " not found") #If key not found 
     
     def delete(self , key): #Delete the key-value pair
         with open(self.defloc) as df: #Read the json file
             data = json.load(df)
         if(key not in data): #If key not found
-            print("Key not found. Invalid Delete operation")
+            print("Key "  + str(key) + " not found. Invalid Delete operation")
         else:
             del (data[key]) #If key is found
-            print("Succesfully deleted")
+            print("Key " +  str(key) + " succesfully deleted")
             with open(self.defloc , 'w') as df:
                 data = json.dump(data , df) #Write data without the deleted key
 
@@ -97,11 +99,14 @@ class store:
 
 if __name__ == "__main__":
     cls = store("C:/Users/PREMRAJ/Desktop/crd_operations/data_store.json") #define the location of the file (or) keep it as "store(None)"
-    cls.create("kumar" , {"age":20 , "height": 170 , "ttl" : 2})
-    cls.create("raj" , {"age":80 , "height": 10 , "ttl" : 10})
-    cls.create("bheem" , {"age":80 , "height": 10 , "ttl" : 5})
+    cls.create("kumar" , {"age":20 , "height": 170}) #cls.create("key" ,"Json Object")
+    cls.create("raj" , {"age":80 , "height": 10 , "ttl" : 10})  #Example with TTL
+    cls.create("giri" , {"age":80 , "height": 10 , "ttl" : 5})
     cls.create("rajesh" , {"age":71 , "height": 10 })
-    cls.read("raj")
+    cls.read("raj") #Data Read Example
+    cls.delete("kumar") #Delete Example
+    cls.delete("kumar") #Delete for unknown data 
     t1 = threading.Thread(cls.ttl()) #Run a separate thread for TTL detection
     t1.start()
+    
     
